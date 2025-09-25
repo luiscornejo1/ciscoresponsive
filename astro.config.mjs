812414b919ce9f4890d87ca3c7cac076b1369205
Luis <you@example.com>
2025-09-25 16:53:07 -0500

    agregando config para github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://luiscornejo1.github.io', // ← Cambiarás esto después
-  base: 'ciscoresponsive.github.io', // ← Cambiarás esto después
+  base: 'ciscoresponsive', // ← Cambiarás esto después
   output: 'static', // ← Para sitios estáticos
   build: {
     assets: 'assets'
